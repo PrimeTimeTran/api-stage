d04b40fa22c3efd5c332e25b3c5d456981a30209
Loi Tran <lvt15@my.fsu.edu>
2018-05-11 15:31:18 +0800

    Add Navbar, controller, base component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
+
 import './App.css';
+import NavbarContainer from './components/NavbarContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+          <NavbarContainer />
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={12} md={12} style={{ backgroundColor: 'yellow' }}>
+                Welcome!
+              </Col>
+            </Row>
+          </Grid>
+        </div>
     );
   }
 }
