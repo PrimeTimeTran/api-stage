f4b4a2ce8f770009a134567f5629db48a2fea08d
Loi Tran <lvt15@my.fsu.edu>
2018-05-18 12:59:08 +0800

    Add Client Headers for all requests. GET, POST, DESTROY"

--- client/src/actions/PostActions.js
+++ client/src/actions/PostActions.js
@@ -1,41 +1,37 @@
-import axios from 'axios';
-
 import { API_ROOT } from '../constants/API-CONFIG.js';
 
 import {
   POST_INITIAL,
   POST_REMOVE,
   POST_ADD
-} from './types'
+} from './types';
 
-export const removePost = (postId) => {
-  axios.delete(`${API_ROOT}posts/${postId}`)
-  return (dispatch) => {
-    dispatch({ type: POST_REMOVE, payload: postId })
+import { client } from './client';
+const request = client();
+
+export const addPost = (post) => {
+  return(dispatch) => {
+    request.post(`${API_ROOT}posts`, { post })
+    .then(response => {
+      dispatch({ type: POST_ADD, payload: response.data })
+    })
   }
 }
 
 export const initialPosts = () => {
-
   return (dispatch) => {
-    axios.get(`${API_ROOT}posts`)
+    request.get(`${API_ROOT}posts`)
       .then(response => {
-        console.log('Response: ', response)
         dispatch({ type: POST_INITIAL, payload: response.data })
       })
   }
 }
 
-export const addPost = (post) => {
-    return(dispatch) => {
-    axios({
-      method: 'post',
-      url: `${API_ROOT}posts`,
-      data: {
-        post
-      }
-    }).then(response => {
-        dispatch({ type: POST_ADD, payload: response.data })
-    })
+export const removePost = (postId) => {
+  request.delete(`${API_ROOT}posts/${postId}`)
+  return (dispatch) => {
+    dispatch({ type: POST_REMOVE, payload: postId })
   }
 }
+
+
