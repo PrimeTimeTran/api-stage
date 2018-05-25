4ea3e6bcfc50072280817dc5cd4ddb61a0ecfbe1
Loi Tran <lvt15@my.fsu.edu>
2018-05-25 15:38:03 +0800

    Correct failing sessions specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  attr_reader :current_user
-
   prepend_view_path 'app/views/api/v1/'
-  before_action :authorize_request, :set_session, except: [:fallback_index_html]
+  before_action :authorize_request, except: [:fallback_index_html]
 
   def fallback_index_html
     render :file => 'public/index.html'
@@ -14,10 +12,6 @@ class ApplicationController < ActionController::API
   private
     def authorize_request
       @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-    end
-
-    def set_session
-      session[:user_id] = current_user.id
     end
 
     def login(user)
