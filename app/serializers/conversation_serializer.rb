57bed6a0301551abe0dc24d7fa2d8d5fca4e105e
Loi Tran <lvt15@my.fsu.edu>
2018-06-02 14:05:34 +0800

    Fixing date format for front end

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -6,7 +6,12 @@ class ConversationSerializer < ApplicationSerializer
              :name
 
   def last_message
-    object.messages.last
+    message = object.messages.last
+    {
+      body: message.body,
+      sent_at: time(message.created_at)
+    }
+
   end
 
   def last_message_from_user
@@ -19,5 +24,11 @@ class ConversationSerializer < ApplicationSerializer
 
   def other_users
     object.users.select {|user| user.id != current_user.id }
+  end
+
+  def time(a)
+    return a.strftime('%I:%M %p') if a > 1.day.ago
+    return a.strftime('%a') if a > 7.days.ago
+    a.strftime('%m-%d')
   end
 end
