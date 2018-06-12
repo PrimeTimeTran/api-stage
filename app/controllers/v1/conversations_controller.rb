1e054cc80328a472ef4917e481792b53131fc128
Loi Tran <lvt15@my.fsu.edu>
2018-06-12 15:08:49 +0800

    Add conversations#show

--- app/controllers/v1/conversations_controller.rb
+++ app/controllers/v1/conversations_controller.rb
@@ -9,5 +9,16 @@ module V1
       users = Conversation.find(params[:id]).users
       render json: users
     end
+
+    def show
+      conversation = Conversation.find(params[:id])
+      user_count = conversation.users.count
+      stage_name = conversation.name
+
+      render json: {
+        user_count: user_count,
+        stage_name: stage_name
+      }
+    end
   end
 end
