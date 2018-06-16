a1e1bce11debbb49fce5c1fd90122458dfa83d77
Loi Tran <lvt15@my.fsu.edu>
2018-06-17 02:49:04 +0700

    Correct last message. Remove comments

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -7,7 +7,6 @@ class AuthorizeApiRequest
     @headers = headers
   end
 
-  # Service entry point - return valid user object
   def call
     {
       user: user
@@ -21,19 +20,16 @@ class AuthorizeApiRequest
   def user
     @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
   rescue ActiveRecord::RecordNotFound => e
-    # raise custom error
     raise(
       ExceptionHandler::InvalidToken,
       ("#{Info.invalid_token} #{e.message}")
     )
   end
 
-  # decode authentication token
   def decoded_auth_token
     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
   end
 
-  # check for token in `Authorization` header
   def http_auth_header
     return headers['Authorization'].split(' ').last if headers['Authorization'].present?
     raise(ExceptionHandler::MissingToken, Info.missing_token)
