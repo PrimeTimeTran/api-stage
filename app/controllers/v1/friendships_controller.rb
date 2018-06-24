77150b32da746c3d67da01469a654dd861faee8f
Loi Tran <lvt15@my.fsu.edu>
2018-06-24 14:25:27 +0800

    WIP removing association from serialization

--- app/controllers/v1/friendships_controller.rb
+++ app/controllers/v1/friendships_controller.rb
@@ -2,7 +2,11 @@ module V1
   class FriendshipsController < ApplicationController
     def index
       friends = current_user.friends
-      render json: ActiveModel::Serializer::CollectionSerializer.new(friends, each_serializer: UserSerializer, except: [:uploads])
+      # Could you help me remove an association from the serialization here? Don't need uploads
+      # render json: ActiveModel::Serializer::CollectionSerializer.new(friends, each_serializer: UserSerializer, except: [:uploads])
+      # render json: ActiveModel::Serializer::CollectionSerializer.new(friends, serializer: UserSerializer, except: [:uploads])
+      # render json: ActiveModel::Serializer::CollectionSerializer.new(friends, serializer: UserSerializer)
+      render json: friends
     end
   end
 end
