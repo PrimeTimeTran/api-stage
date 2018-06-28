326c252e019558cdd87debbe6c3ded2c27121339
Loi Tran <lvt15@my.fsu.edu>
2018-06-28 17:14:23 +0700

    WIP: Uploading Photos endpoint

--- app/controllers/v1/uploads_controller.rb
+++ app/controllers/v1/uploads_controller.rb
@@ -2,5 +2,14 @@
 
 module V1
   class UploadsController < ApplicationController
+    def create
+      image = params[:data]["_parts"].first
+      upload = current_user.uploads.create(uploadable_type: ProfilePhoto, uploadable_id: current_user.id, media_type: 'img')
+      upload.media.attach(
+        io: File.open(image),
+        filename: 'ProfilePhoto.jpg',
+        content_type: 'application/jpg'
+      )
+    end
   end
 end
